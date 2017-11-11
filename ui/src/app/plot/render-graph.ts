1da40cb79e025ac56008bd81ac3995ad5fe8a804
Anurag Kumar <anuragkr.bk@gmail.com>
2017-11-11 21:21:55 +0530

    add function to display lines on hover

--- ui/src/app/plot/render-graph.ts
+++ ui/src/app/plot/render-graph.ts
@@ -1,33 +1,44 @@
 import * as d3 from 'd3';
 
+function calculateCenter(c) {
+  const date = c['judgement_date'].split('-');
+  const year = parseInt(date[0].slice(-2));
+  const month = parseInt(date[1]);
+
+  return {
+    cx: 10 * year,
+    cy: 50 * month,
+  }
+}
+
 export function renderGraph(cases) {
-  const caseCircles = d3.select('#graph')
-    .selectAll('g')
+  d3.select('#graph')
+    .selectAll('circle')
     .data(cases)
     .enter()
-    .append('g')
-    .attr('class', 'case')
-    // .append('circle')
-    // .attr('')
-    .attr('transform', c => {
-      const date = c['judgement_date'].split('-');
-      const year = 10 * (+date[0].slice(-2));
-      const month = 50 * (+date[1]);
-      return `translate(${year}, ${month})`;
-    })
-    .on('mouseover', (c) => {
-      console.log('Mouse enter: ', c)
-      console.log('current el', d3.select(this))
-      d3.select(this).raise()
-        .append('text')
-        .attr('class', 'case-title')
-        .text(c['title'])
-    })
-
-  caseCircles.append('circle')
-    .attr('r', c => 2 * c['mentiones'].length)
+    .append('circle')
+    .attr('id', c => `circle-${c['id']}`)
+    .attr('cx', c => calculateCenter(c).cx)
+    .attr('cy', c => calculateCenter(c).cy)
+    .attr('r', c => 2.5 * c['mentiones'].length)
     .attr('fill', 'grey')
+    .on('mouseover', c => {
+      const circle = d3.select(`#circle-${c['id']}`);
+      const mentionedIn = c['mentioned_in'];
 
-
-  // .text(c => `${c['judgement_date']}, World!`);
+      mentionedIn.forEach(mention => {
+        const target = d3.select(`#circle-${mention}`)
+        console.log(target);
+        d3.select('#graph')
+          .append('line')
+          .attr('x1', circle.attr('cx'))
+          .attr('y1', circle.attr('cy'))
+          .attr('x2', target.attr('cx'))
+          .attr('y2', target.attr('cy'))
+          .attr('style', 'stroke:rgb(255,0,0);stroke-width:2')
+      })
+    })
+    .on('mouseout', () => {
+      d3.selectAll('line').remove()
+    })
 }
