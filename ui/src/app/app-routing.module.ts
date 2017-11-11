ef30c548b55fecabca2eabb85b26276b8b513101
Anurag Kumar <anuragkr.bk@gmail.com>
2017-11-11 19:22:30 +0530

    create route config for plots

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PlotComponent } from './plot/plot.component';
+
+const routes: Routes = [
+  { path: '', component: PlotComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
