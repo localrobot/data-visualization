80495c5a3d19d3863cb6ac0f756a4565d957659a
Anurag Kumar <anuragkr.bk@gmail.com>
2017-11-11 21:21:33 +0530

    remove unnessary code

--- ui/src/app/plot/plot.component.ts
+++ ui/src/app/plot/plot.component.ts
@@ -10,30 +10,10 @@ import { renderGraph } from './render-graph';
 })
 export class PlotComponent implements OnInit {
 
-  cases: any;
-
   constructor(private plotService: PlotService) { }
 
   ngOnInit() {
-    this.plotService.getCases().subscribe(cases => {
-      this.cases = cases;
-      console.log(cases);
-      // this.findYearLimits(cases);
-      // this.renderGraph(cases);
-      renderGraph(cases);
-    })
-  }
-
-  findYearLimits(cases) {
-    let min = 9999, max = -1;
-    for (let c of cases) {
-      let year = parseInt(c['judgement_date'].split('-')[0]);
-      min = year < min ? year : min;
-      max = year > max ? year : max;
-    }
-
-    console.log(min, max);
-    return [min, max];
+    this.plotService.getCases().subscribe(cases => renderGraph(cases))
   }
 
 }
