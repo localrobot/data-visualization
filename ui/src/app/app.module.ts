267942e09909177901b97126a9174f96f6fd02ab
Anurag Kumar <anuragkr.bk@gmail.com>
2017-11-11 19:23:12 +0530

    add imports and configure app module

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatCardModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlotComponent, PlotService } from './plot';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlotComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    PlotService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
